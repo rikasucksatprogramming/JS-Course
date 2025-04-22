d6db4a37ca68742fc46dc516accf31b1cfb20e0b
Rika <r.mayama124@gmail.com>
2025-04-21 20:43:39 -0400

    Not a lot done today, but here is some JS string slicing, which I keep on thinking is splicing

--- scripts.js
+++ scripts.js
@@ -1,62 +1,22 @@
-// string methods = allows you to manipulate and work with text (strings)
+// string splicing = creating a substring from a portion of another string
+// I don't see how useful this is..
+const fullName = "Eugene Cruz";
 
-let userName = "rika_sucks  ";
+let firstName = fullName.slice(0, 3);
+let lastName = fullName.slice(7, 11);
+console.log(lastName)
 
-// charAt()
-userName.charAt(0)
-console.log(userName.charAt(0));
+console.log(firstName);
 
-// indexOF()
-userName.indexOf("o");
-console.log(userName.indexOf("s"));
+let nameFirst = fullName.slice(0,fullName.indexOf(" "));
+console.log(nameFirst);
 
-// legenth 
+// Example 
 
-console.log(userName.length)
+const email = "example@example.com";
 
-// trim()
-userName = userName.trim();
-
-console.log(userName);
-
-// toUpperCase and toLowerCase
-
-userName = userName.toUpperCase();
-console.log(userName);
-
-// repeat
-userName = userName.repeat(3);
-console.log(userName);
-
-// startwith() and endsWith()
-
-let reuslt = userName.startsWith(" ");
- if(reuslt){
-    console.log("You're Username cannot have spaces!");
- }
- else{
-    console.log(`You're in, ${userName}`);
- }
-
- // includes()
-
- let sm = userName.includes(" ");
- if(sm){
-    console.log("You're Username cannot include spaces!");
- }
- else{
-    console.log(`You're in, ${userName}`);
- }
-
- let phoneNumber = "718-967-2311";
-
- phoneNumber = phoneNumber.replaceAll("-", "");
- console.log(phoneNumber);
-
- 
- // padStart()/ padEnd(); Put this "0" as many necesary times at the start/end until there are 15 charecters
- let numberPhone = "718-967-2311";
-
- numberPhone = numberPhone.padStart(15, "0");
- console.log(numberPhone);
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@"));
+console.log(username);
+console.log(extension);
 
